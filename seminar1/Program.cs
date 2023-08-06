c98043735630f621dfdb7b2ad86286a6581832b5
YuliyaMatveeva <yu-mat@mail.ru>
2023-08-06 18:14:35 +0400

    Сохранение домашнего задания по семинару 1 комментарий

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -116,6 +116,7 @@ else
 //Task 8. Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
 
+/*
 Console.Write("Input a numder ");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -129,3 +130,5 @@ while(current <= num)
     }
     current++;
 }
+*/
+
